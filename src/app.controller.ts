7e3dab224bb30409f400aef9af055a87955ca7a8
Dmitry <daniloff200@gmail.com>
2018-12-26 16:02:40 +0200

    feat(zoho): add build message for vacations and remote work

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -47,16 +47,17 @@ export class AppController {
             reasonMsg = ` Reason: ${query.reason} `
         }
 
-        if (query.from && query.to) {
+        if (query.from && query.to && (query.from !== query.to)) {
             console.log('day off from to')
             dayOffMsg = `${query.firstName} ${query.lastName} took dayoffs from ${query.from} to ${query.to}. ${reasonMsg}`;
         }
 
         webhook.send(dayOffMsg, function (err, res) {
+
             if (err) {
-                console.log('Error:', err);
+                console.log('Error sending day off message:', err);
             } else {
-                console.log('Message sent: ', res);
+                console.log('Dayoff message  sent: ', res);
             }
         });
     }
@@ -73,26 +74,65 @@ export class AppController {
             reasonMsg = ` Reason: ${query.reason} `
         }
 
-        if (query.from && query.to) {
+        if (query.from && query.to && (query.from !== query.to)) {
             console.log('sick from to')
             sickLeaveMsg = `${query.firstName} ${query.lastName} took sick leaves from ${query.from} to ${query.to}. ${reasonMsg}`;
         }
 
         webhook.send(sickLeaveMsg, function (err, res) {
+
             if (err) {
-                console.log('Error:', err);
+                console.log('Error sending sick leave message:', err);
             } else {
-                console.log('Message sent: ', res);
+                console.log('Sick leave message  sent: ', res);
             }
         });
     }
 
     buildRemoteWorkMessage(query) {
+        console.log('im here3')
+        const url = 'https://hooks.slack.com/services/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?authtoken=xoxb-459079151415-459237191175-RiYCxKYkMniFzhR9g4goBxE0';
+        const webhook = new IncomingWebhook(url);
+        let reasonMsg = '';
+        let sickLeaveMsg = `${query.firstName} ${query.lastName} works remotely at ${query.from}. ${reasonMsg}`;
 
+        if (query.reason) {
+            console.log('reason for remote')
+            reasonMsg = ` Reason: ${query.reason} `
+        }
+
+        if (query.from && query.to && (query.from !== query.to)) {
+            console.log('remote from to')
+            sickLeaveMsg = `${query.firstName} ${query.lastName} works remotely from ${query.from} to ${query.to}. ${reasonMsg}`;
+        }
+
+        webhook.send(sickLeaveMsg, function (err, res) {
+            if (err) {
+                console.log('Error sending remote message:', err);
+            } else {
+                console.log('Remote message  sent: ', res);
+            }
+        });
     }
 
     buildVacationMessage(query) {
+        console.log('im here4')
+        const url = 'https://hooks.slack.com/services/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?authtoken=xoxb-459079151415-459237191175-RiYCxKYkMniFzhR9g4goBxE0';
+        const webhook = new IncomingWebhook(url);
+        let vacationMsg = `${query.firstName} ${query.lastName} took vacation day at ${query.from}.`;
 
+        if (query.from && query.to && (query.from !== query.to)) {
+            console.log('vac from to')
+            vacationMsg = `${query.firstName} ${query.lastName} took vacation from ${query.from} to ${query.to}.`;
+        }
+
+        webhook.send(vacationMsg, function (err, res) {
+            if (err) {
+                console.error('Error sending vacation message:', err);
+            } else {
+                console.log('Vacation message sent: ', res);
+            }
+        });
     }
 
 }
