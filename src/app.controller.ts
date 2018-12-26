0803eb7b9afeca62bcfe91a7ff11c4424e986773
Dmitry <daniloff200@gmail.com>
2018-12-26 15:22:19 +0200

    feat(zoho): add sick leave event, change messages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,23 +4,88 @@ import { IncomingWebhook } from '@slack/client';
 
 @Controller()
 export class AppController {
-  @Get('/slack-integrations')
-  async root(
-    @Req() req,
-    @Res() res,
-    @Query() query
-  ): Promise<any> {
-    const url = 'https://hooks.slack.com/services/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?authtoken=xoxb-459079151415-459237191175-RiYCxKYkMniFzhR9g4goBxE0'
-    const webhook = new IncomingWebhook(url);
+    @Get('/slack-integrations')
+    async root(
+        @Req() req,
+        @Res() res,
+        @Query() query
+    ): Promise<any> {
 
-    webhook.send(`${query.firstName} ${query.lastName} took dayoff at ${query.from}`, function(err, res) {
-      if (err) {
-        console.log('Error:', err);
-      } else {
-        console.log('Message sent: ', res);
-      }
-    });
+        this.checkLeaveType(query);
 
-    return res.status(HttpStatus.OK).send(query);
-  }
+        return res.status(HttpStatus.OK).send(query);
+    }
+
+    checkLeaveType(query) {
+        switch (query.type) {
+            case 'Day Off':
+                this.buildDayOffMessage();
+                break;
+            case 'Remote Work':
+                this.buildRemoteWorkMessage();
+                break;
+            case 'Sick Leave':
+                this.buildSickLeaveMessage();
+                break;
+            case 'Vacation':
+                this.buildVacationMessage();
+                break;
+        }
+
+    }
+
+    buildDayOffMessage() {
+        const url = 'https://hooks.slack.com/services/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?authtoken=xoxb-459079151415-459237191175-RiYCxKYkMniFzhR9g4goBxE0';
+        const webhook = new IncomingWebhook(url);
+        let reasonMsg = '';
+        let dayOffMsg = `${query.firstName} ${query.lastName} took dayoff at ${query.from}. ${reasonMsg}`;
+
+        if (query.reason) {
+             reasonMsg = ` Reason: ${query.reason} `
+        }
+
+        if (query.from && query.to) {
+            dayOffMsg = `${query.firstName} ${query.lastName} took dayoffs from ${query.from} to ${query.to}. ${reasonMsg}`;
+        }
+
+        webhook.send(dayOffMsg, function (err, res) {
+            if (err) {
+                console.log('Error:', err);
+            } else {
+                console.log('Message sent: ', res);
+            }
+        });
+    }
+
+    buildSickLeaveMessage() {
+        const url = 'https://hooks.slack.com/services/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?authtoken=xoxb-459079151415-459237191175-RiYCxKYkMniFzhR9g4goBxE0';
+        const webhook = new IncomingWebhook(url);
+        let reasonMsg = '';
+        let sickLeaveMsg = `${query.firstName} ${query.lastName} took sick leave at ${query.from}. ${reasonMsg}`;
+
+        if (query.reason) {
+            reasonMsg = ` Reason: ${query.reason} `
+        }
+
+        if (query.from && query.to) {
+            sickLeaveMsg = `${query.firstName} ${query.lastName} took sick leaves from ${query.from} to ${query.to}. ${reasonMsg}`;
+        }
+
+        webhook.send(sickLeaveMsg, function (err, res) {
+            if (err) {
+                console.log('Error:', err);
+            } else {
+                console.log('Message sent: ', res);
+            }
+        });
+    }
+
+    buildRemoteWorkMessage() {
+
+    }
+
+    buildVacationMessage() {
+
+    }
+
 }
